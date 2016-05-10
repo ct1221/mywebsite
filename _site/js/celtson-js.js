48917f8fd64fb2bbcce7cfcf817903b1d9cc9d00
Celtson <celtson.011@gmail.com>
2016-05-10 16:06:56 -0500

    Added touch move
    Added a touch move event and wanted to see how it works out on my
    iPhone

--- _site/js/celtson-js.js
+++ _site/js/celtson-js.js
@@ -1,21 +1,20 @@
 // Home Page //
 
-Zepto(function($){
+Zepto(function($) {
 
-  var verse_button = $('.verse .button');
-  // Makes sure Zepto adds class, so functoin only works if Zepto exists.
-  // "hide" class is so that div does not show up on page load.
-  $('.pop-up').addClass('hide');
-  $('.pop-up').addClass('active-jq');
-  $('.close').on('click', function (e) {
+    var verse_button = $('.verse .button');
+    // Makes sure Zepto adds class, so functoin only works if Zepto exists.
+    // "hide" class is so that div does not show up on page load.
+    $('.pop-up').addClass('hide');
     $('.pop-up').addClass('active-jq');
-    verse_button.removeClass('active');
-  })
+    $('.close').on('click', function(e) {
+        $('.pop-up').addClass('active-jq');
+        verse_button.removeClass('active');
+    })
 
-  $(document).on('click', '.verse .button', function (e) {
-    $('.pop-up').removeClass('hide');
-    $('.pop-up').toggleClass('active-jq');
-    $(this).toggleClass('active');
-  })
-  $('.verse .button').on('click', function() {});
+    verse_button.on('click touchmove', function(e) {
+        $('.pop-up').removeClass('hide');
+        $('.pop-up').removeClass('active-jq');
+        $(this).addClass('active');
+    })
 })
