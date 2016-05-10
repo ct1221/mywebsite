5fceec0fd2c6cca0e21f6ea490823adeb9867d9e
Celtson <celtson.011@gmail.com>
2016-05-09 23:16:25 -0500

    Added js for verse
    Added Zepto Js files and custom js for the verse pop up

---
+++ _site/js/celtson-js.js
@@ -0,0 +1,20 @@
+// Home Page //
+
+Zepto(function($){
+
+  var verse_button = $('.verse .button');
+  // Makes sure Zepto adds class, so functoin only works if Zepto exists.
+  // "hide" class is so that div does not show up on page load.
+  $('.pop-up').addClass('hide');
+  $('.pop-up').addClass('active-jq');
+  $('.close').on('click', function (e) {
+    $('.pop-up').addClass('active-jq');
+    verse_button.removeClass('active');
+  })
+
+  $('.verse .button').on('click', function (e) {
+    $('.pop-up').removeClass('hide');
+    $('.pop-up').toggleClass('active-jq');
+    $(this).toggleClass('active');
+  })
+})
